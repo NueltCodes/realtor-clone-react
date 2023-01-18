ed810df66e8020df465c19d845cc7dd9391e69ec
NueltCodes <olaniranemmanuelt.com>
2023-01-18 16:35:21 +0300

    created auth page, components and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <div className="text-blue-400 text-xl">Header</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+
+        {/* <Profile /> */}
+        {/* <Offers />
+      <Signin />
+      <SignUp />
+      <ForgotPassword /> */}
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
+
+const Forgotpasword = () => {
+  const [email, setEmail] = useState("");
+
+  return (
+    <sesction>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="Key"
+            className="w-full rounded-2xl "
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email Address"
+              className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Have a account?
+                <Link
+                  to="/sign-in"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+
+              <p>
+                <Link
+                  to="/sign-in"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Reset Password
+            </button>
+            <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </sesction>
+  );
+};
+
+export default Forgotpasword;
